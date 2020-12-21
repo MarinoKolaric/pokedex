0f905a684778d882f6e48b0adc2232d487fdad00
Marino Kolaric <marinokolaric@gmail.com>
2020-12-21 17:54:02 +0100

    Add "send home" when clicking on logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import logo from "@images/logo.png";
 import { theme } from "@style";
 
@@ -10,7 +11,7 @@ const HeaderComponent = styled.header`
   border-bottom: 2px solid ${theme.color.gray};
 `;
 
-const LogoWrapper = styled.div`
+const LogoWrapper = styled(Link)`
   width: 120px;
   padding: 0 12px;
 `;
@@ -21,18 +22,15 @@ const Logo = styled.img`
 
 const Title = styled.h1`
   font-size: ${theme.fontSize.lg};
-
 `;
 
 export const Header = () => {
   return (
     <HeaderComponent>
-      <LogoWrapper>
+      <LogoWrapper to="/">
         <Logo src={logo} alt="pokeball logo"></Logo>
       </LogoWrapper>
-      <Title>
-        Pokedex
-      </Title>
+      <Title>Pokedex</Title>
     </HeaderComponent>
   );
 };
